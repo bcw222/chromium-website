53687561b38ef88b20c3ac1dd359ec63ab8cda9b
sStr kl <sreeramklthehindu@gmail.com>
2023-07-21 13:51:41 +0000

    rt
    Change-Id: Ib0b51e6465191f3d960cc1e73cb5a7f10dbc5ab8

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2021 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# found in the LICENSE file.s
 
 """Top-level presubmit script for the Git repo backing chromium.org.
 
